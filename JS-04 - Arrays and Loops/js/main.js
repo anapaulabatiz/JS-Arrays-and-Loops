fb78445cd0b6a24ac4e3a0afd353f942f38d67ce
Ana Paula Batiz <ap.batiz@gmail.com>
2023-03-27 16:44:48 -0600

    Se agregan files html y js

---
+++ JS-04 - Arrays and Loops/js/main.js
@@ -0,0 +1,5 @@
+console.log("****************************Parte 1**************************");
+let names = ["Sofia", "David", "Juan"];
+console.log(names);
+
+console.log("****************************Parte 2**************************");

9dbc3e0eef0c8c642c714083b2200fa870a7ff5f
Ana Paula Batiz <ap.batiz@gmail.com>
2023-03-27 17:22:19 -0600

    Se crean carpetas de Ejerciocio 1 y Ejercicio 2, se completa ejercicio 1

--- JS-04 - Arrays and Loops/Ejercicio 1/js/main.js
+++ JS-04 - Arrays and Loops/Ejercicio 1/js/main.js
@@ -1,6 +1,5 @@
 console.log("****************************Parte 1**************************");
 let names = ["Sofia", "David", "Juan"];
-
 console.log(names);
 
 console.log("****************************Parte 2**************************");
@@ -9,5 +8,6 @@ names.shift();
 console.log(names);
 
 console.log("****************************Parte 3**************************");
-names.splice(1, 0, "Renata")
+names.splice(1, 0, "Renata");
 names.push("Elena");
+console.log(names);

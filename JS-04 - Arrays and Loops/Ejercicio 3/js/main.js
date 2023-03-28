b62ca5db85756b892f29fac2cffbf484f5634ac9
Ana Paula Batiz <ap.batiz@gmail.com>
2023-03-27 23:59:08 -0600

    Se agrega ejercicio 3 completo

--- JS-04 - Arrays and Loops/Ejercicio 3/js/main.js
+++ JS-04 - Arrays and Loops/Ejercicio 3/js/main.js
@@ -1,4 +1,38 @@
 console.log("******************PARTE 1**************************");
-for (xValue = 100; xValue<0; xValue+=5) {
+function x(xValue){
+for (xValue = 10; xValue>0; xValue-=0.5) {
     console.log(xValue); 
-}    
+}
+}
+console.log("******************PARTE 2**************************");
+for (cont = 1; cont<=100; cont+=2){
+    console.log(cont);
+}
+
+//     // if (cont % 2 != 0){
+//         console.log(cont);
+//     }
+// }
+
+console.log("******************PARTE 3**************************");
+function n(nValue){
+    let y = 1;
+    while (y <= nValue) {
+      console.log("["+y+"]");
+      y++;
+    }
+}
+
+console.log("******************PARTE 4**************************");
+function n2(nValue2){
+let i = 0;
+let j = 1;
+function sum (i,j){
+    while(j <= nValue2){
+        i = j + i;
+        j++;
+    }
+    return i;
+} 
+console.log(sum(i,j));
+}
